7b2fcd6f883d7e10833e4c60efa3c567e9515da6
TheTatane <antoine.cherpentier@u-psud.fr>
2017-05-12 16:37:22 +0200

    Test git 2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ import View.Fenetre;
 
 public class Main {
     public static void main (String[] args){
-        Fenetre f = new Fenetre(1000, 800, "Jeux");
+        Fenetre f = new Fenetre(1000, 800, "Jeux Abalone & Dame");
     }
 }
